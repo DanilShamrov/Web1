f2d573ad6e7da0ca96901d20e19caeff43ff0328
DanilShamrov <119667821+DanilShamrov@users.noreply.github.com>
2026-06-20 21:02:03 +0300

    PRE-2

--- js/scriptShop.js
+++ js/scriptShop.js
@@ -7,7 +7,14 @@ function addItem(name, price, img, model){
     sessionStorage.setItem(name+model, JSON.stringify(item))
 }
 function generateCards(){
-    var cards=[["Ремонт двигателя", "100 000₽", "assets/engine.svg"], ["Коробка передач", "50 000₽", "assets/gearbox.svg"], ["Лобовое стекло", "25 000₽", "assets/windshield.svg"], ["Шины зимние", "10 000₽", "assets/tires.svg"], ["Шины летние", "10 000₽", "assets/tires.svg"], ["Тормозные колодки", "20 000₽", "assets/brakes.svg"], ["Фары передние", "5 000₽", "assets/light.svg"], ["Машинное масло", "1 000₽", "assets/oil.svg"]]
+    var cards=[["Ремонт двигателя", "100 000₽", "assets/engine.svg"],
+               ["Коробка передач", "50 000₽", "assets/gearbox.svg"],
+               ["Лобовое стекло", "25 000₽", "assets/windshield.svg"],
+               ["Шины зимние", "10 000₽", "assets/tires.svg"], 
+               ["Шины летние", "10 000₽", "assets/tires.svg"], 
+               ["Тормозные колодки", "20 000₽", "assets/brakes.svg"], 
+               ["Фары передние", "5 000₽", "assets/light.svg"], 
+               ["Машинное масло", "1 000₽", "assets/oil.svg"]]
     for (let i = cards.length - 1; i > 0; i--) {
         const j = Math.floor(Math.random() * (i + 1));
         [cards[i], cards[j]] = [cards[j], cards[i]];
@@ -38,8 +45,23 @@ function createCard(name, price, src){
     el.appendChild(pr)
     a.appendChild(el)
     var btn = document.createElement("button")
-    btn.onclick=()=>{addItem(name, price, img.src, selectedButton)}
-    btn.textContent="В корзину"
+    btn.onclick=()=>{addItem(name, price, src, selectedButton)
+                     btn.textContent="Добавлено";
+                     btn.classList.add("green");
+    }
+    console.log(sessionStorage)
+    console.log(name+selectedButton)
+    console.log(sessionStorage.getItem(name+selectedButton))
+    console.log(sessionStorage.key(0))
+    console.log(sessionStorage.getItem(sessionStorage.key(0)))
+    if(sessionStorage.getItem(name+selectedButton)!=null){
+        btn.textContent="Добавлено";
+        btn.classList.add("green");
+    }
+    else {
+        btn.textContent="В корзину"
+        
+    }
     btn.classList.add("cardButton");
     el.appendChild(btn)
     a.appendChild(el)
